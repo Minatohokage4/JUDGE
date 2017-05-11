74728bed5cffcdc81c32882601f969008477e7b6
Sarawut Nawawisitkul <mamochiro11@gmail.com>
2017-05-11 23:50:16 +0700

    delete

--- teacher/delete_subject.php
+++ teacher/delete_subject.php
@@ -3,15 +3,15 @@ require_once('../functions.php');
 connectdb();
 $subject_id = $_POST["subject_id"];
 
-
-$query = "DELETE FROM `subject` WHERE subject_id = '$subject_id'";
+$query = "DELETE FROM problems1 WHERE event_id in (SELECT event_id FROM subject WHERE subject_id = '$subject_id' )" ;
+$query2 = "DELETE FROM `subject` WHERE subject_id = '$subject_id'";
 $query1 = "DELETE FROM `event` WHERE subject_id = '$subject_id'";
-//$result1 = mysql_query($query1);
-echo $query;
-//$result = mysql_query($query);
+$result = mysql_query($query);
+$result1 = mysql_query($query1);
+$result3 = mysql_query($query2);
 
 
 
-//header("Location: subject_Container.php?delete");
+header("Location: subject_Container.php?delete");
 
 ?>
