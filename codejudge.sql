f72f63010f3e5458efe3a9e6b85e489ca4e6e043
ARM\nantipat <keymasterviriya1150@gmail.com>
2017-05-13 18:42:23 +0700

    update sql

--- codejudge.sql
+++ codejudge.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: May 11, 2017 at 07:39 PM
+-- Generation Time: May 13, 2017 at 01:40 PM
 -- Server version: 10.1.19-MariaDB
 -- PHP Version: 5.6.28
 
@@ -37,8 +37,8 @@ CREATE TABLE `event` (
 --
 
 INSERT INTO `event` (`event_id`, `event_name`, `subject_id`) VALUES
-(1, 'Quiz_OO', '977-001'),
-(3, 'Quiz_CPT', '977-002');
+(1, 'Quiz_OOP', '977-100'),
+(2, 'Mid', '977-100');
 
 -- --------------------------------------------------------
 
@@ -74,15 +74,71 @@ CREATE TABLE `problems` (
   `text` text NOT NULL,
   `input` text NOT NULL,
   `output` text NOT NULL,
-  `time` int(11) NOT NULL DEFAULT '3000'
+  `time` int(11) NOT NULL DEFAULT '3000',
+  `event_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `problems`
 --
 
-INSERT INTO `problems` (`sl`, `name`, `text`, `input`, `output`, `time`) VALUES
-(1, 'n+2', 'INPUT \r\nN\r\nOUTPUT N+2', '2', '4', 1000);
+INSERT INTO `problems` (`sl`, `name`, `text`, `input`, `output`, `time`, `event_id`) VALUES
+(1, 'n+2', 'INPUT \r\nN\r\nOUTPUT N+2', '2', '4', 1000, 1),
+(2, 'jQuery tabSlideOut Demo', '1000', '', '', 1000, 1),
+(3, 'testint', 'sdfdsfsd', 'dsfsdf', 'sdfdsf', 1000, 2),
+(4, 'testString', 'dsfsdf', 'asdsad', 'asdad', 1000, 2),
+(5, 'WASAS', 'asasA', 'ASDSAD', 'ASDASD', 10001, 1),
+(6, 'asdsad', 'sdadad', 'asdsad', 'asdasd', 1000, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `profile`
+--
+
+CREATE TABLE `profile` (
+  `sl` varchar(50) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `lastname` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `profile`
+--
+
+INSERT INTO `profile` (`sl`, `name`, `lastname`) VALUES
+('5', 'sarawut', 'nawawisitkul'),
+('6', 'sarawut5', 'nawawisitkul'),
+('7', 'sutita', 'saraya');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `regis`
+--
+
+CREATE TABLE `regis` (
+  `student_id` varchar(50) NOT NULL,
+  `subject_id` varchar(50) NOT NULL,
+  `subject_name` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `regis`
+--
+
+INSERT INTO `regis` (`student_id`, `subject_id`, `subject_name`) VALUES
+('7', '977-669', 'IT'),
+('7', '977-002', 'CPT'),
+('5', '977-003', 'database'),
+('5', '977-669', 'IT'),
+('5', '977-002', 'CPT'),
+('5', '977-000', 'test'),
+('5', '977-123', 'client'),
+('5', '977-888', 'Os'),
+('6', '977-003', 'database'),
+('6', '977-001', 'OOP'),
+('5', '977-001', 'OOP');
 
 -- --------------------------------------------------------
 
@@ -98,20 +154,18 @@ CREATE TABLE `solve` (
   `attempts` int(11) NOT NULL DEFAULT '1',
   `soln` text NOT NULL,
   `filename` varchar(25) NOT NULL,
-  `lang` varchar(20) NOT NULL,
-  `event_id` int(11) NOT NULL
+  `lang` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `solve`
 --
 
-INSERT INTO `solve` (`sl`, `problem_id`, `username`, `status`, `attempts`, `soln`, `filename`, `lang`, `event_id`) VALUES
-(1, 1, 'arm', 2, 2, 'asdsad', 'test.c', 'c', 1),
-(2, 2, 'arm', 2, 2, 'asdsad', 'test.c', 'c', 2),
-(3, 3, 'aziz', 1, 3, '#include<iostream>\r\nusing namespace std;\r\nint main()\r\n{\r\n    cout <<"hello";\r\n    return 0;\r\n}', 'test.c', 'c', 2),
-(4, 1, 'aziz', 1, 3, '#include<iostream>\r\nusing namespace std;\r\nint main()\r\n{\r\n    cout <<"hello";\r\n    return 0;\r\n}', 'test.c', 'c', 0),
-(5, 2, 'aziz', 1, 1, '#include<iostream>\r\nusing namespace std;\r\nint main()\r\n{\r\n    cout <<"hello";\r\n    return 0;\r\n}', 'sdsd.c', 'c', 0);
+INSERT INTO `solve` (`sl`, `problem_id`, `username`, `status`, `attempts`, `soln`, `filename`, `lang`) VALUES
+(2, 1, 'arm', 2, 2, 'asdsad', 'test.c', 'c'),
+(4, 4, 'mark', 2, 2, 'aaa', 'mark.java', 'java'),
+(5, 6, 'mark', 1, 1, 'asdsadsa', 'mark.c', 'c'),
+(6, 2, '5730213064', 2, 1, 'texxx', 'test.c', 'c');
 
 -- --------------------------------------------------------
 
@@ -130,16 +184,11 @@ CREATE TABLE `subject` (
 --
 
 INSERT INTO `subject` (`subject_id`, `subject_name`, `Teacher_id`) VALUES
-('', 'GG', '3'),
-('8786-88', 'sa', '3'),
-('977-000', 'test', '3'),
-('977-001', 'OOP', '3'),
 ('977-002', 'CPT', '2'),
-('977-003', 'database', '3'),
+('977-100', 'OOP', '3'),
 ('977-123', 'client', '3'),
-('977-669', 'IT', '5'),
-('977-888', 'Os', '3'),
-('980-779', 'DDE', '3');
+('977-555', 'database3', '3'),
+('977-669', 'IT', '5');
 
 -- --------------------------------------------------------
 
@@ -164,9 +213,10 @@ CREATE TABLE `users` (
 INSERT INTO `users` (`sl`, `username`, `salt`, `hash`, `email`, `status`, `type`) VALUES
 (1, 'admin', 'bmu2w', 'bmkVMsQ70yhfc', 'keymasterviriya1150@gmail.com', 1, 1),
 (2, 'arm', '5i5i2', '5itZ3xhef4QLI', 'keymasterviriya1150@gmail.com', 1, 0),
-(3, 'aziz', '56vu4', '56706V.vDo81k', '', 1, 1),
+(3, 'aziz', '56vu4', '56706V.vDo81k', '', 1, 0),
 (5, 'mark', 'cnenz', 'cn8pK1Cu6sf3s', 'mamochiro11@gmail.com', 1, 0),
-(6, '5730213064', 'db1n4', 'db2/cCxQW5Qyw', 'mamochiro11@gmail.com', 1, 0);
+(6, '5730213064', 'db1n4', 'db2/cCxQW5Qyw', 'mamochiro11@gmail.com', 1, 0),
+(7, 'fang', 'fik87', 'fionu3giiS71.', 'fag@gmail.com', 1, 0);
 
 --
 -- Indexes for dumped tables
@@ -184,6 +234,12 @@ ALTER TABLE `event`
 -- Indexes for table `problems`
 --
 ALTER TABLE `problems`
+  ADD PRIMARY KEY (`sl`);
+
+--
+-- Indexes for table `profile`
+--
+ALTER TABLE `profile`
   ADD PRIMARY KEY (`sl`);
 
 --
@@ -212,38 +268,22 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `event`
 --
 ALTER TABLE `event`
-  MODIFY `event_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `event_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT for table `problems`
 --
 ALTER TABLE `problems`
-  MODIFY `sl` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `sl` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 --
 -- AUTO_INCREMENT for table `solve`
 --
 ALTER TABLE `solve`
-  MODIFY `sl` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `sl` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `sl` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `event`
---
-ALTER TABLE `event`
-  ADD CONSTRAINT `event_ibfk_1` FOREIGN KEY (`subject_id`) REFERENCES `subject` (`subject_id`);
-
---
--- Constraints for table `problems`
---
-ALTER TABLE `problems`
-  ADD CONSTRAINT `problems_ibfk_1` FOREIGN KEY (`sl`) REFERENCES `event` (`event_id`);
-
+  MODIFY `sl` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
